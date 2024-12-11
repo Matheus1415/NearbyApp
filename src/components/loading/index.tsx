6504ad4e1198f31c2982860b0b3975dc1b1696e3
Matheus <mps.web.tech@gmail.com>
2024-12-11 11:15:35 -0300

    Clear

--- src/components/loading/index.tsx
+++
@@ -1,6 +0,0 @@
-import { ActivityIndicator } from "react-native";
-import { colors } from "@/styles/theme"
-import { styles } from "./style";
-export default function Loading() {
-    return <ActivityIndicator color={colors.green.base} style={styles.container} />
-}
